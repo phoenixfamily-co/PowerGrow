47f507ed7b0ec5d753a786efb9b369716e83f5a3
lotfinasab <abbas.lotfinasab@utechia.com>
2023-09-23 12:00:31 +0330

    access token stored

--- User/views.py
+++ User/views.py
@@ -64,7 +64,7 @@ def pass_view(request, number):
 
 def home_view(request):
     about = AboutUs.objects.values().first()
-    template = loader.get_template('public/home.html')
+    template = loader.get_template('user/home.html')
     context = {
         "logo": about["logo"],
     }
