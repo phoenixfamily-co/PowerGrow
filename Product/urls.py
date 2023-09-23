9647ce090560ef943aeb39c7952f13d375b4caf6
lotfinasab <abbas.lotfinasab@utechia.com>
2023-09-23 06:50:33 +0330

    home page bug fixed

--- Product/urls.py
+++ Product/urls.py
@@ -4,7 +4,7 @@ from Product.views import CourseView, sport_view, product_view, DaysView
 app_name = 'product'
 
 urlpatterns = [
-    path('api/course/', CourseView.as_view({'post': 'create'}), name='courses'),
+    path('api/course/', CourseView.as_view({'post': 'create', 'get': 'list'}), name='courses'),
     path('api/day/', DaysView.as_view({'post': 'create'}), name='days'),
     path('<int:pk>/', product_view, name='product'),
     path('category/', sport_view, name='category'),
