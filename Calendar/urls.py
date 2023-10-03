d8e1d8b5e9b3bfd32bb39a8f42325b57bfef6bab
lotfinasab <abbas.lotfinasab@utechia.com>
2023-10-04 00:43:37 +0330

    calendar added

--- Calendar/urls.py
+++ Calendar/urls.py
@@ -8,10 +8,10 @@ from django.conf import settings
 app_name = 'calendar'
 
 urlpatterns = [
-    path('api/year/', YearView.as_view({'post': 'create', 'get': 'list'}), name='calendar'),
-    path('api/month/', MonthView.as_view({'post': 'create', 'get': 'list'}), name='calendar'),
-    path('api/day/', DayView.as_view({'post': 'create', 'get': 'list'}), name='calendar'),
-    path('api/time/', TimeView.as_view({'post': 'create', 'get': 'list'}), name='calendar'),
+    path('api/year/', YearView.as_view({'post': 'create', 'get': 'list'}), name='year'),
+    path('api/month/', MonthView.as_view({'post': 'create', 'get': 'list'}), name='month'),
+    path('api/day/', DayView.as_view({'post': 'create', 'get': 'list'}), name='day'),
+    path('api/time/', TimeView.as_view({'post': 'create', 'get': 'list'}), name='time'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
