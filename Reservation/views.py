146375784d5f40045cf5e32dbf358ba591e0adc0
lotfinasab <abbas.lotfinasab@utechia.com>
2023-10-05 20:54:33 +0330

    startDay added to month

--- Reservation/views.py
+++ Reservation/views.py
@@ -16,8 +16,8 @@ def reservation_view(request):
     gym = Gym.objects.values().first()
     sport = Sport.objects.all().values()
     year = Year.objects.all().values()
-    month = Month.objects.get(id=6)
-    day = Day.objects.filter(month=6).values()
+    month = Month.objects.get(id=7)
+    day = Day.objects.filter(month=7).values()
     template = loader.get_template('public/reservation.html')
     context = {
         "about": about,
