476f07bdfe137d306caddf9b70822f6c95420d99
lotfinasab <abbas.lotfinasab@utechia.com>
2023-10-15 02:51:43 +0330

    reservation bug fixed

--- User/serializer.py
+++ User/serializer.py
@@ -36,14 +36,6 @@ class AdminRegisterSerializer(serializers.ModelSerializer):
         user.set_password(validated_data['password'])
         user.save()
 
-        # data = {'from': '5000xxx', 'to': validated_data['number'], 'text':
-        #     'سلام به باشگاه ورزشی حجاب خوش امدید'
-        #     f'نام کاربری : "{validated_data["password"]}$" '
-        #     f'رمز عبور : "{validated_data["number"]}$"'
-        #     f'لطفا بعد از ورود به پنل کاربری رمز عبور خود را تغییر دهید'}
-        # requests.post('https://console.melipayamak.com/api/send/simple/d15bf0639e874ecebb5040b599cb8af6',
-        #               json=data)
-
         return user
 
 
