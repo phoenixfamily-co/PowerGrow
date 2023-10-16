b4f0ac505e57708836f26a7bb1ef239e7803992d
lotfinasab <abbas.lotfinasab@utechia.com>
2023-10-16 23:08:58 +0330

    login type of user fixed

--- PowerGrow/settings.py
+++ PowerGrow/settings.py
@@ -79,8 +79,6 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10,
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
 
 }
