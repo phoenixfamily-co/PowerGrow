90f14c1eaa24fb6ea63f40d3267fa6cba43eabd3
lotfinasab <abbas.lotfinasab@utechia.com>
2023-10-21 04:36:13 +0330

    exit button added

--- User/serializer.py
+++ User/serializer.py
@@ -39,6 +39,7 @@ class AdminRegisterSerializer(serializers.ModelSerializer):
         data = {'from': '50004001047208', 'to': validated_data['number'], 'text': 'سلام به باشگاه ورزشی حجاب خوش امدید'
                                                                                   f'نام کاربری: {validated_data["number"]}'
                                                                                   f'رمز عبور:{validated_data["password"]}'
+                                                                                  f'https://powergrow.net/'
                 }
         response = requests.post('https://console.melipayamak.com/api/send/simple/d15bf0639e874ecebb5040b599cb8af6',
                                  json=data)
