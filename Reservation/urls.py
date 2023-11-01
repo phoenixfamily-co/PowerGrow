ce5a8e3b542466c4433856eca89ff5cd9c863491
lotfinasab <abbas.lotfinasab@utechia.com>
2023-11-01 21:35:04 +0330

    exit button added

--- Reservation/urls.py
+++ Reservation/urls.py
@@ -10,6 +10,8 @@ urlpatterns = [
     path('api/reserve/', ReservationView.as_view({'post': 'create', 'get': 'list'}), name='reserve'),
     path('api/admin/reserve/', ManagerAddReservationView.as_view({'post': 'create', 'get': 'list'}),
          name='admin-reserve'),
+    path('api/admin/reserve/<int:pk>/', ManagerAddReservationView.as_view({'delete': 'destroy'}),
+         name='delete-admin-reserve'),
     path('api/gym/', GymView.as_view({'post': 'create', 'get': 'list'}), name='gym'),
     path('transaction/<int:gym>/<int:time>/<int:session>/<str:holiday>/', transaction_view, name='transaction'),
     path('successful/<int:gym>/<int:time>/<int:session>/<str:holiday>/', successful_view, name='successful'),
