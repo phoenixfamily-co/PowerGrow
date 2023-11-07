84007e5fd74cd50fb45c9e1cff7be92da086731e
lotfinasab <abbas.lotfinasab@utechia.com>
2023-11-07 09:27:31 +0330

    user panel edited

--- Seo/urls.py
+++ Seo/urls.py
@@ -7,5 +7,6 @@ app_name = 'seo'
 urlpatterns = [
 
     path('api/news/', NewsApi.as_view({'post': 'create', 'get': 'list'}), name='news'),
+    path('api/news/<int:pk>/', NewsApi.as_view({'delete': 'destroy'}), name='destroy-news'),
 
 ]
