5e024bd646c76706cb90c2560712e532e4f51358
lotfinasab <abbas.lotfinasab@utechia.com>
2023-12-02 21:19:11 +0330

    user otp bugs fixed

--- Reservation/views.py
+++ Reservation/views.py
@@ -7,6 +7,22 @@ from About.models import AboutUs
 from Product.models import Sport
 from Reservation.models import *
 from Reservation.serializer import GymSerializer, ReservationSerializer, AdminReservationSerializer
+from django.conf import settings
+import requests
+import json
+
+
+if settings.SANDBOX:
+    sandbox = 'sandbox'
+else:
+    sandbox = 'www'
+
+ZP_API_REQUEST = f"https://{sandbox}.zarinpal.com/pg/rest/WebGate/PaymentRequest.json"
+ZP_API_VERIFY = f"https://{sandbox}.zarinpal.com/pg/rest/WebGate/PaymentVerification.json"
+ZP_API_STARTPAY = f"https://{sandbox}.zarinpal.com/pg/StartPay/"
+
+merchant = 'd2f95f64-0068-4d7c-a5d1-53f13d810abd'
+CallbackURL = 'http://127.0.0.1:8080/reservation/verify/'
 
 
 def reservation_view(request):
@@ -29,6 +45,7 @@ def transaction_view(request, gym, time, session, holiday):
     gym = Gym.objects.filter(id=gym).values().first()
     times = Time.objects.get(id=time)
     sport = Sport.objects.all().values()
+    amount = times.price
     template = loader.get_template('public/transaction.html')
     context = {
         "about": about,
@@ -134,3 +151,52 @@ class ManagerAddReservationView(viewsets.ModelViewSet):
         except Http404:
             pass
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+def send_request(request, amount, description, phone):
+    data = {
+        "MerchantID": merchant,
+        "Amount": amount,
+        "Description": description,
+        "Phone": phone,
+        "CallbackURL": CallbackURL,
+    }
+    data = json.dumps(data)
+    # set content length by data
+    headers = {'content-type': 'application/json', 'content-length': str(len(data))}
+    try:
+        response = requests.post(ZP_API_REQUEST, data=data, headers=headers, timeout=10)
+
+        if response.status_code == 200:
+            response = response.json()
+            if response['Status'] == 100:
+                return {'status': True, 'url': ZP_API_STARTPAY + str(response['Authority']),
+                        'authority': response['Authority']}
+            else:
+                return {'status': False, 'code': str(response['Status'])}
+        return response
+
+    except requests.exceptions.Timeout:
+        return {'status': False, 'code': 'timeout'}
+    except requests.exceptions.ConnectionError:
+        return {'status': False, 'code': 'connection error'}
+
+
+def verify(authority, amount):
+    data = {
+        "MerchantID": merchant,
+        "Amount": amount,
+        "Authority": authority,
+    }
+    data = json.dumps(data)
+    # set content length by data
+    headers = {'content-type': 'application/json', 'content-length': str(len(data))}
+    response = requests.post(ZP_API_VERIFY, data=data, headers=headers)
+
+    if response.status_code == 200:
+        response = response.json()
+        if response['Status'] == 100:
+            return {'status': True, 'RefID': response['RefID']}
+        else:
+            return {'status': False, 'code': str(response['Status'])}
+    return response
