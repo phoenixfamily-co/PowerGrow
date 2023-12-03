97854feafce7549acb9f67065b107fc4b4c9ea55
lotfinasab <abbas.lotfinasab@utechia.com>
2023-12-03 14:51:20 +0330

    user otp bugs fixed

--- Reservation/views.py
+++ Reservation/views.py
@@ -20,7 +20,7 @@ else:
 ZP_API_REQUEST = f"https://{sandbox}.zarinpal.com/pg/rest/WebGate/PaymentRequest.json"
 ZP_API_VERIFY = f"https://{sandbox}.zarinpal.com/pg/rest/WebGate/PaymentVerification.json"
 ZP_API_STARTPAY = f"https://{sandbox}.zarinpal.com/pg/StartPay/"
-description = "توضیحات مربوط به تراکنش را در این قسمت وارد کنید"  # Required
+description = "رزرو سالن چند منظور حجاب"  # Required
 phone = 'YOUR_PHONE_NUMBER'  # Optional
 # Important: need to edit for realy server.
 CallbackURL = 'http://127.0.0.1:8080/reservation/verify/'
