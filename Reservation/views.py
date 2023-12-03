f2d5401f67cb82eee0da74d993a061bb54536f2b
lotfinasab <abbas.lotfinasab@utechia.com>
2023-12-03 14:09:44 +0330

    user otp bugs fixed

--- Reservation/views.py
+++ Reservation/views.py
@@ -11,7 +11,6 @@ from django.conf import settings
 import requests
 import json
 
-
 if settings.SANDBOX:
     sandbox = 'sandbox'
 else:
@@ -21,7 +20,10 @@ ZP_API_REQUEST = f"https://{sandbox}.zarinpal.com/pg/rest/WebGate/PaymentRequest
 ZP_API_VERIFY = f"https://{sandbox}.zarinpal.com/pg/rest/WebGate/PaymentVerification.json"
 ZP_API_STARTPAY = f"https://{sandbox}.zarinpal.com/pg/StartPay/"
 
-merchant = 'd2f95f64-0068-4d7c-a5d1-53f13d810abd'
+amount = 1000  # Rial / Required
+description = "توضیحات مربوط به تراکنش را در این قسمت وارد کنید"  # Required
+phone = 'YOUR_PHONE_NUMBER'  # Optional
+# Important: need to edit for realy server.
 CallbackURL = 'http://127.0.0.1:8080/reservation/verify/'
 
 
@@ -45,7 +47,6 @@ def transaction_view(request, gym, time, session, holiday):
     gym = Gym.objects.filter(id=gym).values().first()
     times = Time.objects.get(id=time)
     sport = Sport.objects.all().values()
-    amount = times.price
     template = loader.get_template('public/transaction.html')
     context = {
         "about": about,
@@ -153,12 +154,12 @@ class ManagerAddReservationView(viewsets.ModelViewSet):
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
-def send_request(request, amount):
+def send_request(request):
     data = {
-        "MerchantID": merchant,
+        "MerchantID": settings.MERCHANT,
         "Amount": amount,
-        "Description": "",
-        "Phone": "",
+        "Description": description,
+        "Phone": phone,
         "CallbackURL": CallbackURL,
     }
     data = json.dumps(data)
@@ -182,9 +183,9 @@ def send_request(request, amount):
         return {'status': False, 'code': 'connection error'}
 
 
-def verify(authority, amount):
+def verify(authority):
     data = {
-        "MerchantID": merchant,
+        "MerchantID": settings.MERCHANT,
         "Amount": amount,
         "Authority": authority,
     }
