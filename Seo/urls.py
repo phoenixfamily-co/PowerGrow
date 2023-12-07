261494ae04551efeab9ec7ce399b9b94b8175747
lotfinasab <abbas.lotfinasab@utechia.com>
2023-12-07 03:50:10 +0330

    user otp bugs fixed

--- Seo/urls.py
+++ Seo/urls.py
@@ -6,7 +6,9 @@ app_name = 'seo'
 
 urlpatterns = [
 
-    path('api/news/', NewsApi.as_view({'post': 'create', 'get': 'list'}), name='news'),
+    path('news/', news_view, name='news_view'),
+    path('api/news/create/', NewsApi.as_view({'post': 'create'}), name='create_news'),
+    path('api/news/', NewsApi.as_view({'get': 'list'}), name='news'),
     path('api/news/<int:pk>/', NewsApi.as_view({'delete': 'destroy'}), name='destroy-news'),
 
 ]
--- Seo/views.py
+++ Seo/views.py
@@ -1,7 +1,21 @@
+from django.http import HttpResponse
+from django.template import loader
 from rest_framework import viewsets
 
+from About.models import AboutUs
 from Seo.models import News
 from Seo.serializer import NewsSerializer
+
+
+def news_view(request):
+    about = AboutUs.objects.values().first()
+    news = News.objects.all().values()
+    template = loader.get_template('manager/news.html')
+    context = {
+        "news": news,
+        "about": about,
+    }
+    return HttpResponse(template.render(context, request))
 
 
 class NewsApi(viewsets.ModelViewSet):
