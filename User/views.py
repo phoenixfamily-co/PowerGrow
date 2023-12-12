a491e5ac7663e3b727d4ecf238f1c9580daa05b8
lotfinasab <abbas.lotfinasab@utechia.com>
2023-12-12 20:11:54 +0330

    admin panel completed

--- User/views.py
+++ User/views.py
@@ -182,7 +182,7 @@ def admin_user_view(request):
 def teacher_user_day(request, pk, day):
     about = AboutUs.objects.values().first()
     template = loader.get_template('teacher/users.html')
-    participants = Course.objects.get(id=pk) & Course.objects.get(participants__day__title="یکشنبه")
+    participants = Course.objects.get(id=pk, participants__day__title="یکشنبه")
     context = {
         "about": about,
         "participants" : participants
