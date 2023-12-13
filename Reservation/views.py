6e27ad412cdd8e0b0b5dfeac3d7779abc973d72d
lotfinasab <abbas.lotfinasab@utechia.com>
2023-12-13 23:40:22 +0330

    admin panel completed

--- Reservation/views.py
+++ Reservation/views.py
@@ -264,4 +264,4 @@ def verify(request):
     #     else:
     #         return JsonResponse({'status': False})
 
-    return JsonResponse({"Authority": request.GET.get('Authority', '')})
+    return JsonResponse(response)
