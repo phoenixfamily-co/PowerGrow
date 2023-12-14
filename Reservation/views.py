1365146594bef0fea5ee034a55ead9e805285078
lotfinasab <abbas.lotfinasab@utechia.com>
2023-12-14 04:00:10 +0330

    admin panel completed

--- Reservation/views.py
+++ Reservation/views.py
@@ -179,7 +179,7 @@ class ReservationView(viewsets.ModelViewSet):
                     authority=str(response_data['Authority']),
                     success=False
                 )
-                return redirect(ZP_API_STARTPAY + str(response['Authority']))
+                return Response({'redirect': ZP_API_STARTPAY + str(response['Authority'])})
             else:
                 return Response({'error': 'Payment request failed'})
         except json.JSONDecodeError:
