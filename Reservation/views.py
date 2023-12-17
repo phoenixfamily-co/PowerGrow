6854c326ffa0d8662f33cf95f32536bcb6eaea22
lotfinasab <abbas.lotfinasab@utechia.com>
2023-12-17 20:17:05 +0330

    admin panel completed

--- Reservation/views.py
+++ Reservation/views.py
@@ -238,7 +238,8 @@ def verify(request):
         template = loader.get_template('public/successful.html')
         reservation.success = True
         sliced_queryset = Time.objects.filter(day__name=reservation.time.day.name, time=reservation.time.time,
-                                          day__month__number__gte=reservation.time.day.month.number).order_by(
+                                              day__number__gte=reservation.time.day.number,
+                                              day__month__number__gte=reservation.time.day.month.number).order_by(
             'day__number').order_by('day__month__number')[:int(reservation.session)]
         # time = Time.objects.filter(id__in=sliced_queryset).update(reserved=True)
         reservation.save()
