978878e4f528b37941546459c86b0de5a85327ba
lotfinasab <abbas.lotfinasab@utechia.com>
2023-12-18 02:15:55 +0330

    admin panel completed

--- Reservation/views.py
+++ Reservation/views.py
@@ -243,7 +243,7 @@ def verify(request):
         start_index = time_list.index(reservation.time.id)
         sliced_queryset = Time.objects.filter(day__name=reservation.time.day.name, time=reservation.time.time,
                                               day__month__number__gte=reservation.time.day.month.number).order_by(
-            'day__month__number')[start_index:reservation.session]
+            'day__month__number')[:start_index:reservation.session]
         # time = Time.objects.filter(id__in=sliced_queryset).update(reserved=True)
         reservation.save()
         return Response(sliced_queryset.values())
