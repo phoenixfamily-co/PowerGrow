6685236b82e9f04dff31bfb29099545bd753c8c9
lotfinasab <abbas.lotfinasab@utechia.com>
2023-12-17 19:53:37 +0330

    admin panel completed

--- Reservation/views.py
+++ Reservation/views.py
@@ -237,9 +237,10 @@ def verify(request):
     if response['Status'] == 100:
         template = loader.get_template('public/successful.html')
         reservation.success = True
-        month = Month.objects.filter(number__gte=reservation.time.day.month.number)
-        time_sorted = Time.objects.filter(day__name=reservation.time.day.name, time=reservation.time.time).order_by('-day__number').order_by('-day__month__number')[0:reservation.session]
-        time = Time.objects.filter(id__in=time_sorted).update(reserved=True)
+        sliced_queryset = Time.objects.filter(day__name=reservation.time.day.name, time=reservation.time.time,
+                                          day__month__number__gte=reservation.time.day.month.number).order_by(
+            '-day__number').order_by('-day__month__number')
+        time = Time.objects.filter(id__in=sliced_queryset).update(reserved=True)
         reservation.save()
         return Response(time)
         # return HttpResponse(template.render(context, request))
