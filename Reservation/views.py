b1eb5e898a544c12dcc762163dc5462d14963715
lotfinasab <abbas.lotfinasab@utechia.com>
2023-12-18 03:49:22 +0330

    admin panel completed

--- Reservation/views.py
+++ Reservation/views.py
@@ -201,11 +201,12 @@ class ManagerAddReservationView(viewsets.ModelViewSet):
         return Response(serializer.data)
 
     def destroy(self, request, *args, **kwargs):
-        reservation = Reservations.objects.get(id=kwargs.get('pk'))
-        ids = Time.objects.filter(day__name=reservation.time.day.name, time=reservation.time.time,
-                                  day__month__number__gte=reservation.time.day.month.number) \
-                  .exclude(day__month__number=reservation.time.day.month.number,
-                           day__number__lt=reservation.time.day.number) \
+        reservation = Reservations.objects.get(id=self.kwargs['pk'])
+        time = Time.objects.get(id=kwargs.get('time'))
+        ids = Time.objects.filter(day__name=time.day.name, time=time.time,
+                                  day__month__number__gte=time.day.month.number) \
+                  .exclude(day__month__number=time.day.month.number,
+                           day__number__lt=time.day.number) \
                   .order_by('day__month__number').values_list('pk', flat=True)[:int(reservation.session)]
         Time.objects.filter(pk__in=list(ids)).update(reserved=True)
         try:
