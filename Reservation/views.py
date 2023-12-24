d48148acab46831008bfbb46db62f9e523210ded
lotfinasab <abbas.lotfinasab@utechia.com>
2023-12-24 05:06:02 +0330

    admin panel completed

--- Reservation/views.py
+++ Reservation/views.py
@@ -273,20 +273,9 @@ def generate_pdf_file(request, reservation):
     buffer = BytesIO()
     p = canvas.Canvas(buffer)
     p.setFont('BYekan', 12)
-    p.drawString(100, 100, get_farsi_bulleted_text("تاریخ:", 10))
+    p.drawString(100, 100,"تاریخ:")
     p.showPage()
     p.save()
     buffer.seek(0)
 
     return FileResponse(buffer, as_attachment=True, filename=f"{reservation}.pdf")
-
-
-def get_farsi_bulleted_text(text, wrap_length=None):
-    farsi_text = text
-    if wrap_length:
-        line_list = textwrap.wrap(farsi_text, wrap_length)
-        line_list.reverse()
-        line_list[0] = '{} &#x02022;'.format(line_list[0])
-        farsi_text = '<br/>'.join(line_list)
-        return '<font>%s</font>' % farsi_text
-    return '<font>%s &#x02022;</font>' % farsi_text
