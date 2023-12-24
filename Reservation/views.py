454a68bd94a52dad2e14e274becf4c59cdc9d34e
lotfinasab <abbas.lotfinasab@utechia.com>
2023-12-24 05:29:34 +0330

    admin panel completed

--- Reservation/views.py
+++ Reservation/views.py
@@ -272,7 +272,7 @@ def generate_pdf_file(request, reservation):
     p = canvas.Canvas(buffer)
     p.setFont('BYekan', 12)
     p.setPageSize(A4)
-    p.drawRightString(500, 700, text_converter("تاریخ:"))
+    p.drawRightString(550, 900, text_converter("تاریخ:"))
     p.showPage()
     p.save()
     buffer.seek(0)
