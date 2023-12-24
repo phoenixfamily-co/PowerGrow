182a316ee544f5073592d323f5e5f6d2311fab7a
lotfinasab <abbas.lotfinasab@utechia.com>
2023-12-24 16:58:59 +0330

    admin panel completed

--- Reservation/views.py
+++ Reservation/views.py
@@ -273,7 +273,7 @@ def generate_pdf_file(request, pk):
     reservation = Reservations.objects.get(id=pk)
     startDate = f"{reservation.time.day.month.year.number}/{reservation.time.day.month.number}/{reservation.time.day.number}"
     endDate = f'{Time.objects.get(pk=reservation.endDate).day.month.year.number}/{Time.objects.get(pk=reservation.endDate).day.month.number}/{Time.objects.get(pk=reservation.endDate).day.number}'
-    endTime = (dt.datetime.combine(dt.date(1, 1, 1), reservation.time.time)+datetime.timedelta(minutes=90))
+    endTime = (dt.datetime.combine(dt.date(1, 1, 1), reservation.time.time)+datetime.timedelta(minutes=90)).time()
 
     buffer = BytesIO()
     p = canvas.Canvas(buffer)
