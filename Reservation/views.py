a6151d068cc2c2e93708bcc9a7e02ff47bbc3375
lotfinasab <abbas.lotfinasab@utechia.com>
2023-12-25 17:07:20 +0330

    admin panel completed

--- Reservation/views.py
+++ Reservation/views.py
@@ -214,6 +214,7 @@ class ManagerAddReservationView(viewsets.ModelViewSet):
                   .order_by('day__month__number').values_list('pk', flat=True)[:int(session)]
         Time.objects.filter(pk__in=list(ids)).update(reserved=True)
         serializer = ReservationSerializer(reservations)
+        serializer.is_valid()
         instance = serializer.save()
         end = Reservations.objects.get(pk=instance.id)
         end.endDate = int(ids.last())
