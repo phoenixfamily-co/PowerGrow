96c16349ff69ff281d532cce23394575c181a99e
abbas lotfinasab <abbas.lotfinasab2024@gmail.com>
2024-04-19 19:01:36 +0330

    all feild added

--- Product/admin.py
+++ Product/admin.py
@@ -1,13 +1,13 @@
-from django.contrib import admin
 from Product.models import *
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
 from .models import Participants
-
 admin.site.register(Course)
 admin.site.register(Days)
 admin.site.register(Sport)
+
+UserAdmin.search_fields = ('name', 'number')
 
 
 class ParticipantsAdmin(admin.ModelAdmin):
--- User/admin.py
+++ User/admin.py
@@ -1,10 +1,7 @@
 from django.contrib import admin
-from User.models import *
-from django.contrib import admin
 from django.contrib.auth.models import User
 
 admin.site.register(User)
 
-UserAdmin.search_fields = ('name', 'number')
 
 
