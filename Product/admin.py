0c5a5c9239380a469491c71bd7eb89da2817c88a
abbas lotfinasab <abbas.lotfinasab2024@gmail.com>
2024-04-20 15:32:10 +0330

    all feild added

--- Product/admin.py
+++ Product/admin.py
@@ -4,9 +4,7 @@ from .models import *
 
 @admin.register(Participants)
 class ParticipantsAdmin(admin.ModelAdmin):
-    def formfield_for_foreignkey(self, db_field, request, **kwargs):
-        if db_field.name == "user":
-            kwargs["queryset"] = User.objects.filter(name__in=['God', 'Demi God'])
-        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+    list_display = ('name', 'user')
+    list_filter = ('user__name',)
 
 
