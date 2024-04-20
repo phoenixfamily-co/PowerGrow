342d54127f13811fc67d082c9154022024a0efcc
abbas lotfinasab <abbas.lotfinasab2024@gmail.com>
2024-04-20 15:33:42 +0330

    all feild added

--- User/admin.py
+++ User/admin.py
@@ -2,9 +2,7 @@ from django.contrib import admin
 from User.models import User
 
 
+@admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     list_display = ('name', 'number')
     search_fields = ('name', 'number')
-
-
-admin.site.register(User, UserAdmin)
