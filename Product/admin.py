b8011425e4dc1f313a4bd50bd46e662a8f7a8b5a
abbas lotfinasab <abbas.lotfinasab2024@gmail.com>
2024-04-21 02:21:58 +0330

    all feild added

--- Product/admin.py
+++ Product/admin.py
@@ -2,9 +2,9 @@ from django.contrib import admin
 from .models import *
 
 
-@admin.register(Participants)
 class ParticipantsAdmin(admin.ModelAdmin):
     model = Participants
     autocomplete_fields = ['user']
 
 
+admin.site.register(Participants, ParticipantsAdmin)
