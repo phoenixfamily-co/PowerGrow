abc8c9fe6a73faeb64f0fc66aa66503fdb763bab
abbas lotfinasab <abbas.lotfinasab2024@gmail.com>
2024-04-24 04:50:57 +0330

    all feild added

--- Calendar/views.py
+++ Calendar/views.py
@@ -70,7 +70,7 @@ class DayView(viewsets.ModelViewSet):
     queryset = Day.objects.all()
     serializer_class = DaySerializer
 
-    def perform_create(self, serializer):
+    def add_time(self):
         times = list([
             "06:30:00", "08:00:00", "09:30:00", "11:00:00", "12:30:00", "14:00:00", "15:30:00", "17:00:00", "18:30:00",
             "20:00:00", "21:30:00", "23:00:00", "00:30:00"])
