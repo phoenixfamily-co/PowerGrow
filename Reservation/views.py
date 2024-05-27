14f34e37d240a4e473071cd47ff6d5b52f0fb56e
abbas lotfinasab <abbas.lotfinasab2024@gmail.com>
2024-05-27 20:39:06 +0330

    pagination added

--- Reservation/views.py
+++ Reservation/views.py
@@ -289,7 +289,7 @@ def generate_pdf_file(request, pk):
               .exclude(day__month__number=reservation.time.day.month.number,
                        day__number__lt=reservation.time.day.number) \
               .order_by('day__month__number').values_list('pk', flat=True)[:int(reservation.session)]
-    endDateId = Time.objects.filter(pk__in=list(ids)).last()
+    endDateId = Time.objects.filter(pk__in=list(ids)).order_by("day_id").last()
     endDate = f"{endDateId.day.month.year.number}/{endDateId.day.month.number}/{endDateId.day.number}"
 
     buffer = BytesIO()
