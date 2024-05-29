2bd8813da06fa21b5b1931bf009d344cfeff6107
abbas lotfinasab <abbas.lotfinasab2024@gmail.com>
2024-05-30 00:28:38 +0330

    pagination added

--- User/views.py
+++ User/views.py
@@ -179,12 +179,14 @@ def profile_view(request, pk):
 def salary_view(request, pk):
     about = AboutUs.objects.values().first()
     template = loader.get_template('teacher/salary.html')
-    user = User.objects.filter(id=pk).values_list("participants__course__id",flat=True)
-    size = Participants.objects.filter(course__pk__in=user).values()
+    user = User.objects.filter(id=pk).first()
+    participants = Participants.objects.filter(course_id__in=user, user__is_teacher=False,
+                                               user__is_superuser=False, user__is_staff=False,
+                                               price__gt=0).values()
     context = {
         "about": about,
         "user": user,
-        "size": len(list(size)),
+        "size": len(list(participants)),
 
     }
     return HttpResponse(template.render(context, request))
