7ddad64120eed7977839f5cc1b3df788d21e5952
abbas lotfinasab <abbas.lotfinasab2024@gmail.com>
2024-07-01 12:51:54 +0330

    endDate added

--- Seo/models.py
+++ Seo/models.py
@@ -1,6 +1,17 @@
 from django.db import models
 
+from Calendar.models import Day
+from Product.models import Course
+
+NEWS_CHOICE = (
+    ('کنسلی', 'کنسلی'),
+    ('تعطیل', 'تعطیل'),
+    ('بقیه', 'بقیه'),
+)
+
 
 class News(models.Model):
-    title = models.TextField(blank=True, null=True)
+    title = models.CharField(max_length=100, choices=NEWS_CHOICE, default='public')
+    date = models.ForeignKey(Day, on_delete=models.CASCADE, related_name='news', null=True, blank=True)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='news', null=True, blank=True)
     description = models.TextField(blank=True, null=True)
