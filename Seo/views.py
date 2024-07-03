246411e11531c25a9c0f9ad4166ac6b3900bc2fc
abbas lotfinasab <abbas.lotfinasab2024@gmail.com>
2024-07-03 21:26:35 +0330

    endDate added

--- Seo/views.py
+++ Seo/views.py
@@ -1,10 +1,13 @@
 from django.http import HttpResponse
 from django.template import loader
+from flask import Response
 from rest_framework import viewsets
 from rest_framework.decorators import permission_classes
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
 from About.models import AboutUs
+from Calendar.models import Day
+from Product.models import Course
 from Seo.models import News
 from Seo.serializer import NewsSerializer
 
@@ -35,3 +38,16 @@ def admin_news_view(request):
 class NewsApi(viewsets.ModelViewSet):
     queryset = News.objects.all()
     serializer_class = NewsSerializer
+
+    def perform_create(self, serializer):
+        data = self.request.data
+        title = data["title"]
+        description = data["description"]
+        date = Day.objects.filter(id=self.kwargs['course']).first()
+        course = Course.objects.filter(id=data["course"]).first()
+        news = News.objects.update_or_create(title=title, description=description, date=date, course=course)
+
+        serializer = NewsSerializer(news)
+        return Response(serializer.data)
+
+
