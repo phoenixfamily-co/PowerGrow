5a0b7f1f7bf351594c206a8eb480f1a93a0d8597
abbas lotfinasab <abbas.lotfinasab2024@gmail.com>
2024-09-24 02:09:44 +0330

    font awsome updated

--- PowerGrow/middleware.py
+++ PowerGrow/middleware.py
@@ -6,9 +6,8 @@ class SessionAuthMiddleware:
         self.get_response = get_response
 
     def __call__(self, request):
-        if request.user.is_authenticated:
-            # کاربر احراز هویت شده است
-            return self.get_response(request)
-        else:
-            # کاربر احراز هویت نشده است
-            return JsonResponse({'error': 'Authentication required'}, status=401)
+        # بررسی مسیرهای غیر حفاظتی
+        if request.path not in ['/user/login/', '/login/api/', '/', '/home/']:
+            if not request.user.is_authenticated:
+                return JsonResponse({'error': 'Authentication required'}, status=401)
+        return self.get_response(request)
