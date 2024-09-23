5e0c555f7ec187ed84748c436484e0e0d3c6b619
abbas lotfinasab <abbas.lotfinasab2024@gmail.com>
2024-09-24 03:09:06 +0330

    font awsome updated

--- PowerGrow/middleware.py
+++ PowerGrow/middleware.py
@@ -1,13 +1,19 @@
+import re
 from django.http import JsonResponse
 
 
 class SessionAuthMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
+        self.non_auth_paths = ['/user/login/', '/user/login/api/', '/', '/home/', '/sport/']
 
     def __call__(self, request):
         # بررسی مسیرهای غیر حفاظتی
-        if request.path not in ['/user/login/', '/user/login/api/', '/', '/home/', '/sport/']:
-            if not request.user.is_authenticated:
-                return JsonResponse({'error': 'Authentication required'}, status=401)
+        for path in self.non_auth_paths:
+            if request.path.startswith(path) or re.match(r'^/your-path/[\d]+/$', request.path):
+                return self.get_response(request)
+
+        if not request.user.is_authenticated:
+            return JsonResponse({'error': 'Authentication required'}, status=401)
+
         return self.get_response(request)
