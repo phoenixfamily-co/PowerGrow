0f19e8c7014bd0ccd212e9dc7a2f4e1bb2d5fb12
abbas lotfinasab <abbas.lotfinasab2024@gmail.com>
2024-09-28 22:02:38 +0330

    product app clean and improved

--- Product/models.py
+++ Product/models.py
@@ -31,7 +31,7 @@ class Course(models.Model):
     capacity = models.IntegerField(blank=True, null=True)
     gender = models.CharField(max_length=10, choices=GENDER_CHOICE)
     datetime = models.DateTimeField(default=timezone.now)
-    active = models.BooleanField(default=True)
+    active = models.BooleanField(blank=True, null=True)
     sport = models.ForeignKey(Sport, on_delete=models.CASCADE, related_name='courses', null=True, blank=True)
 
 
