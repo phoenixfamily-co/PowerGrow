93e11a03bfca365c5f6d2bcd44ed424f34cbbecd
abbas lotfinasab <abbas.lotfinasab2024@gmail.com>
2024-09-29 05:55:55 +0330

    product app clean and improved

--- Product/admin.py
+++ Product/admin.py
@@ -14,24 +14,23 @@ class SportAdmin(admin.ModelAdmin):
 
 @admin.register(Participants)
 class ParticipantsAdmin(admin.ModelAdmin):
-    list_display = ('title', 'day', 'session', 'startDay', 'endDay', 'user', 'course')  # اینجا نمایش عنوان و روز
+    list_display = ('title', 'day', 'session', 'startDay', 'endDay', 'user', 'course')
     autocomplete_fields = ('day', 'user')  # اضافه کردن قابلیت autocomplete برای فیلد day
-
-    def formfield_for_foreignkey(self, db_field, request, **kwargs):
-        if db_field.name == "startDay":
-            kwargs["queryset"] = Day.objects.all().order_by('-id')  # مرتب‌سازی بر اساس ID
-        return super().formfield_for_foreignkey(db_field, request, **kwargs)
 
     def get_form(self, request, obj=None, **kwargs):
         form = super().get_form(request, obj, **kwargs)
-        if obj:  # اگر یک پارتیسیپنت موجود باشد
-            form.base_fields['day'].queryset = Day.objects.filter(course=obj.course)  # فیلتر روزها بر اساس دوره
-            form.base_fields['session'].queryset = Session.objects.filter(course=obj.course)  # فیلتر جلسات بر اساس دوره
-        elif 'course' in request.GET:  # اگر یک دوره از GET دریافت شده باشد
-            course_id = request.GET['course']
-            form.base_fields['day'].queryset = Day.objects.filter(course_id=course_id)
-            form.base_fields['session'].queryset = Session.objects.filter(course_id=course_id)
-        return form
 
+        if obj:  # اگر یک Participant موجود باشد
+            form.base_fields['day'].queryset = Days.objects.filter(session__course=obj.course)
+            form.base_fields['session'].queryset = Session.objects.filter(course=obj.course)
+        elif 'course' in request.GET:  # اگر دوره‌ای در GET وجود داشته باشد
+            course_id = request.GET['course']
+            form.base_fields['day'].queryset = Days.objects.filter(session__course_id=course_id)
+            form.base_fields['session'].queryset = Session.objects.filter(course_id=course_id)
+        else:  # در غیر این صورت، هیچ روز یا جلسه‌ای را نمایش نده
+            form.base_fields['day'].queryset = Days.objects.none()
+            form.base_fields['session'].queryset = Session.objects.none()
+
+        return form
 
 
