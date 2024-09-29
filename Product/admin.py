f3ad4f170be6ba0abe4242a8f213968049078280
abbas lotfinasab <abbas.lotfinasab2024@gmail.com>
2024-09-29 05:51:42 +0330

    product app clean and improved

--- Product/admin.py
+++ Product/admin.py
@@ -24,9 +24,11 @@ class ParticipantsAdmin(admin.ModelAdmin):
 
     def get_form(self, request, obj=None, **kwargs):
         form = super().get_form(request, obj, **kwargs)
-        if 'course' in request.GET:
+        if obj:  # اگر یک پارتیسیپنت موجود باشد
+            form.base_fields['day'].queryset = Day.objects.filter(course=obj.course)  # فیلتر روزها بر اساس دوره
+            form.base_fields['session'].queryset = Session.objects.filter(course=obj.course)  # فیلتر جلسات بر اساس دوره
+        elif 'course' in request.GET:  # اگر یک دوره از GET دریافت شده باشد
             course_id = request.GET['course']
-            # فقط روزها و جلسات مرتبط با دوره خاص را نشان می‌دهد
             form.base_fields['day'].queryset = Day.objects.filter(course_id=course_id)
             form.base_fields['session'].queryset = Session.objects.filter(course_id=course_id)
         return form
