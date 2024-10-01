685f2a46a639a9beac3692e6e06845233da1be9a
abbas lotfinasab <abbas.lotfinasab2024@gmail.com>
2024-10-01 11:07:24 +0330

    product app clean and improved

--- Product/models.py
+++ Product/models.py
@@ -39,7 +39,7 @@ class Course(models.Model):
                               verbose_name='ورزش')
 
     def __str__(self):
-        return self.title  # به جای ID، نام ورزش را نمایش می‌دهد
+        return f"{self.pk}-{self.title}"  # به جای ID، نام ورزش را نمایش می‌دهد
 
 
 class Session(models.Model):
