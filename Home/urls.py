95d2037c65ef8b61123f53347b21d684c3d1c9fb
abbas lotfinasab <abbas.lotfinasab2024@gmail.com>
2024-10-05 03:09:19 +0330

    product app clean and improved

--- Home/urls.py
+++ Home/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('', home_view, name='home'),
     path('slider/', slider_view, name='slider_view'),
     path('api/slider/create/', SliderView.as_view({'post': 'create'}), name='create_slider'),
+    path('api/slider/update/<int:pk>/', SliderView.as_view({'put': 'update'}), name='update_slider'),
     path('api/slider/', SliderView.as_view({'get': 'list'}), name='slider'),
     path('api/slider/<int:pk>/', SliderView.as_view({'delete': 'destroy'}), name='delete-slider')
 
