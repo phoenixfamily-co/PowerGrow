95d4d55d6e4a1506c23fd9deec472e5437301651
abbas lotfinasab <abbas.lotfinasab2024@gmail.com>
2024-10-10 04:57:25 +0330

    teacher panel updated

--- Seo/models.py
+++ Seo/models.py
@@ -11,7 +11,7 @@ NEWS_CHOICE = (
 
 
 class News(models.Model):
-    title = models.CharField(max_length=100, choices=NEWS_CHOICE, default='public')
-    date = models.ForeignKey(Day, on_delete=models.CASCADE, related_name='news', null=True, blank=True)
-    course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='news', null=True, blank=True)
-    description = models.TextField(blank=True, null=True)
+    title = models.CharField(max_length=100, choices=NEWS_CHOICE, default='public', verbose_name="موضوع")
+    date = models.ForeignKey(Day, on_delete=models.CASCADE, related_name='news', null=True, blank=True, verbose_name="تاریخ")
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='news', null=True, blank=True, verbose_name="دوره")
+    description = models.TextField(blank=True, null=True, verbose_name="توضیحات")
