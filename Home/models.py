5b68f83da0f091277be49e445df70ecafc9f3889
abbas lotfinasab <abbas.lotfinasab2024@gmail.com>
2024-10-11 07:05:22 +0330

    teacher panel updated

--- Home/models.py
+++ Home/models.py
@@ -6,7 +6,7 @@ from django.utils import timezone
 # Create your models here.
 
 class Slider(models.Model):
-    image = models.ImageField(upload_to="images/", blank=True, null=True)
-    title = models.TextField(blank=True, null=True)
-    description = models.TextField(blank=True, null=True)
+    image = models.ImageField(upload_to="images/", blank=True, null=True, verbose_name="عکس")
+    title = models.TextField(blank=True, null=True, verbose_name="نام")
+    description = models.TextField(blank=True, null=True, verbose_name="توضیحات")
     datetime = models.DateTimeField(default=timezone.now)
--- Reservation/models.py
+++ Reservation/models.py
@@ -6,25 +6,28 @@ from Calendar.models import *
 
 
 class Gym(models.Model):
-    title = models.TextField(blank=True, null=True)
-    image = models.ImageField(upload_to="images/", blank=True, null=True)
-    description = models.TextField(blank=True, null=True)
-    contract = models.FileField(upload_to="files/", blank=True, null=True)
+    title = models.TextField(blank=True, null=True, verbose_name="نام")
+    image = models.ImageField(upload_to="images/", blank=True, null=True, verbose_name="عکس")
+    description = models.TextField(blank=True, null=True, verbose_name="توضیحات")
+    contract = models.FileField(upload_to="files/", blank=True, null=True, verbose_name="قرارداد")
     datetime = models.DateTimeField(default=timezone.now)
 
 
 class Reservations(models.Model):
-    title = models.TextField(blank=True, null=True)
-    description = models.TextField(blank=True, null=True)
-    time = models.ForeignKey(Time, on_delete=models.CASCADE, related_name='reservations_Start', blank=True, null=True)
-    endDate = models.ForeignKey(Time, on_delete=models.CASCADE, related_name='reservations_End', blank=True, null=True)
-    holiday = models.BooleanField(blank=True, null=True)
-    session = models.IntegerField(blank=True, null=True)
-    price = models.IntegerField(blank=True, null=True)
+    title = models.TextField(blank=True, null=True, verbose_name="نام")
+    description = models.TextField(blank=True, null=True, verbose_name="توضیحات")
+    time = models.ForeignKey(Time, on_delete=models.CASCADE, related_name='reservations_Start', blank=True, null=True,
+                             verbose_name="زمان شروع")
+    endDate = models.ForeignKey(Time, on_delete=models.CASCADE, related_name='reservations_End', blank=True, null=True,
+                                verbose_name="زمان پایان")
+    holiday = models.BooleanField(blank=True, null=True, verbose_name="محاسبه تعطیلات")
+    session = models.IntegerField(blank=True, null=True, verbose_name="تعداد جلسات")
+    price = models.IntegerField(blank=True, null=True, verbose_name="قیمت")
     datetime = models.DateTimeField(default=timezone.now)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='reservations',
-                             blank=True, null=True)
-    gym = models.ForeignKey(Gym, on_delete=models.CASCADE, related_name='reservations', null=True, blank=True)
+                             blank=True, null=True, verbose_name="کاربر")
+    gym = models.ForeignKey(Gym, on_delete=models.CASCADE, related_name='reservations', null=True, blank=True,
+                            verbose_name="سالن")
     created = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='reservation',
                                 blank=True, null=True)
     authority = models.TextField(unique=True, blank=True, null=True)
