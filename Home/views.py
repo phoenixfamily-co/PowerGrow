0c1b8c217b35f05374121737b00da8f8106f0800
abbas lotfinasab <abbas.lotfinasab2024@gmail.com>
2024-12-25 03:54:46 +0330

    font awsome updated

--- Home/views.py
+++ Home/views.py
@@ -14,7 +14,7 @@ from PowerGrow.permissions import *
 def home_view(request):
     images = Slider.objects.all().order_by("datetime").values()
     selected = Course.objects.filter(selected=True, active=True).order_by("datetime").values()
-    newDay = Course.objects.filter(newDay=True, active=True).order_by("datetime").values()
+    newDay = Course.objects.filter(newDay=True, active=True).order_by("datetime").values()[:6]
     about = AboutUs.objects.values().first()
     sport = Sport.objects.all().values()
     template = loader.get_template('public/home.html')
